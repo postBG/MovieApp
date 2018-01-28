a49447ed736c4ecd46480dde1856709049c5df42
unknown <profile2697@gmail.com>
2018-01-28 21:48:37 +0900

    add loading states
    - custom함수들은 component가 제공하는 함수와 구별하기 위해 _를 붙여서 선언

--- src/App.js
+++ src/App.js
@@ -34,11 +34,17 @@ class App extends Component {
         }, 5000);
     }
 
+    _renderMovies = () => {
+        const movies = this.state.movies.map((movie, index) => {
+            return <Movie title={movie.title} poster={movie.poster} key={index}/>;
+        });
+
+        return movies;
+    };
+
     render() {
         return <div className="App">
-            {this.state.movies.map((movie, index) => {
-                return <Movie title={movie.title} poster={movie.poster} key={index}/>;
-            })}
+            {this.state.movies ? this._renderMovies() : "Loading"}
         </div>;
     }
 }
