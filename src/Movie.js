83aad8aae3dd8321f58022250b248e26cc13dde2
unknown <profile2697@gmail.com>
2018-01-30 00:59:02 +0900

    Smart vs Dumb component
    - Smart component는 state와 life cycle이 있지만, dumb component는 stateless하고 life cycle도 없다.
    - dumb component는 function 형태로 리팩토링 할 수 있다.

--- src/Movie.js
+++ src/Movie.js
@@ -1,36 +1,28 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
 
-class Movie extends Component {
-    
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    };
-    
-    render() {
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>
-            </div>
-        );
-    }
+function Movie({title, poster}) {
+    return (
+        <div>
+            <MoviePoster poster={poster}/>
+            <h1>{title}</h1>
+        </div>
+    );
 }
 
-class MoviePoster extends Component {
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+};
 
-    static propTypes = {
-        poster: PropTypes.string.isRequired
-    };
-
-    render() {
-        return (
-            <img src={this.props.poster}/>
-        );
-    }
+function MoviePoster({poster}) {
+    return <img src={poster} alt="Movie poster"/>;
 }
+
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
+};
 
 export default Movie;
